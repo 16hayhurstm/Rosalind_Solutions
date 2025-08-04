099a2d2def54190be5d785d43210a534e27b11e9
Myles Patrick Hayhurst <myleshayhurst@Myless-MacBook-Air.local>
2025-08-04 23:57:44 +0100

    Add solution for LEXV

--- LEXV/LEXV.py
+++ LEXV/LEXV.py
@@ -1,33 +1,38 @@
 from itertools import product
+import pyperclip
 
 
-def org_perm(file_loc):
+def LEXV(file_loc):
     # Reading in data and setting variables in correct data type
     with open(file_loc, "r") as file:
         lines = file.readlines()
 
     alpha = lines[0].strip()
     alpha = " ".join(alpha).split()
-    sorted_alpha = sorted(alpha)
-    result = "".join(sorted_alpha)
+    result = "".join(alpha)
 
     n = int(lines[1])
 
     # Making custom dictionary based of input
     order = {ch: i for i, ch in enumerate(result)}
-
+    print(order)
     # creating tupple of all combinations
-    raw_strings = product(result, repeat=n)
+    tups = []
+    for i in range(1, n + 1):
+        raw_strings = product(result, repeat=i)
+        tups += raw_strings
 
     # Converting tupples into lists
-    all_strings = ["".join(p) for p in raw_strings]
+    all_strings = ["".join(p) for p in tups]
 
     # Sorting into lexigraphical order
     sorted_strings = sorted(all_strings, key=lambda s: [order[ch] for ch in s])
 
     # Print each string
-    for s in sorted_strings:
-        print(s)
+    with open("output.txt", "w") as out_file:
+        for s in sorted_strings:
+            print(s)
+            print(s, file=out_file)
 
 
-org_perm("LEXF/rosalind_lexf.txt")
+LEXT("LEXT/rosalind_lexv.txt")
---
+++ LEXV/README.md
@@ -0,0 +1,14 @@
+# 🧬 Rosalind Problem: Ordering Strings of Varying Length Lexicographically (LEXV)
+
+This repository contains my solution to the [LEXV problem on Rosalind](https://rosalind.info/problems/lexv/), which involves generating all possible strings of **varying lengths** from an ordered alphabet, sorted **lexicographically**.
+
+---
+
+## 📚 Problem Description
+
+Given:
+- A **permutation** of up to 12 symbols representing an **ordered alphabet** \(\mathcal{A}\)
+- A positive integer \(n \leq 4\)
+
+Return:
+- All strings of length **at most** \(n\) that can be formed from \(\mathcal{A}\), ordered **lexicographically**, based on the order given in the input (not necessarily standard dictionary order).
